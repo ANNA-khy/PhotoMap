992ec942518a8b34bec0c92c13d3776529ab053b
ANNA-khy <kang01019@naver.com>
2022-01-21 19:40:42 +0900

    spring files

---
+++ Web/PhotoMap/src/test/java/com/example/PhotoMap/PhotoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhotoMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

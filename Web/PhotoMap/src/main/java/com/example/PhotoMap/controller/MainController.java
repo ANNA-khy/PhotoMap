17cf09015d111ff80bf92315cd8b15d379e091e2
ANNA-khy <kang01019@naver.com>
2022-01-24 22:57:20 +0900

    sign up 데이터베이스에 회원정보 저장까지 완료

--- Web/PhotoMap/src/main/java/com/example/PhotoMap/controller/MainController.java
+++ Web/PhotoMap/src/main/java/com/example/PhotoMap/controller/MainController.java
@@ -1,12 +1,23 @@
 package com.example.PhotoMap.controller;
 
 import com.example.PhotoMap.domain.entity.Member;
+import com.example.PhotoMap.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+    private final MemberService memberService;
+
+    /* dependency injection */
+    @Autowired
+    public MainController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/")
     public String home(){
         return "home";
@@ -20,13 +31,25 @@ public class MainController {
         return "members";
     }
     @GetMapping("/members/singup")
-    public String register(Model model){
-        Member member = new Member();
+    public String createForm(Model model){
         return "singup";
     }
+
+    @PostMapping("/members/singup")
+    public String register(MemberForm form){
+        Member member = new Member();
+        member.setId(form.getId());
+        member.setPassword(form.getPassword());
+        member.setEmail(form.getEmail());
+
+        this.memberService.join(member);
+
+        return "/login";
+    }
+
     @GetMapping("/login")
     public String login(Model model){
         Member member = new Member();
-        return "redirect:/";
+        return "login";
     }
 }
--- Web/PhotoMap/src/main/java/com/example/PhotoMap/domain/entity/Member.java
+++ Web/PhotoMap/src/main/java/com/example/PhotoMap/domain/entity/Member.java
@@ -1,4 +1,36 @@
 package com.example.PhotoMap.domain.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id
+    private String id;
+    private String password;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
